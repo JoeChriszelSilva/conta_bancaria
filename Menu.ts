c62de25048b94c4323c6efe9e5d6dd78639d144b
JoeChriszelSilva <joechriszelsilva@gmail.com>
2025-09-01 22:51:07 -0300

    menu 1.0

--- Menu.ts
+++ Menu.ts
@@ -1,5 +1,13 @@
 import readlinesync = require("readline-sync");
 
 export function main (){
-    
+    let option: number;
+    while (true){
+        console.log("********************************************************")
+        console.log("                                                        ")
+        console.log("                CORPORAÇÃO CAPSOLA BANK                 ")
+        console.log("                                                        ")
+        console.log("********************************************************")
+        console.log("                                                        ")
+    }    
 }
